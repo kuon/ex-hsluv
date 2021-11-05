a93475c4442e09466827ca43761e477705fcc333
Nicolas goy <kuon@goyman.com>
2021-11-05 01:21:45 +0100

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Hsluv.MixProject do
   def project do
     [
       app: :hsluv,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
